98fec6240ebe47324749116562438ff5c50b905e
almunna <munna.kuet.me@gmail.com>
2025-06-30 12:46:03 +0600

    hello 1

--- backend/models/Submission.js
+++ backend/models/Submission.js
@@ -3,20 +3,32 @@ import mongoose from "mongoose";
 const submissionSchema = new mongoose.Schema({
   ownerName: { type: String, required: true }, // Owner’s Name
   plotNumber: { type: String, default: "" }, // Plot Number (optional)
-  address: { type: String, required: true }, // Village. Town, District, Region
+  address: { type: String, required: true }, // Village, Town, District, Region
   landUse: { type: String, required: true }, // Land Use
+
+  email: { type: String, required: true }, // Your Email Address
+  agentEmail: { type: String, default: "" }, // Agent's Email (optional)
+  notes: { type: String, default: "" }, // Additional Notes (optional)
 
   coordinates: {
     lat: { type: Number, required: false }, // Optional
     lon: { type: Number, required: false }, // Optional
   },
 
-  documents: [
-    {
+  documents: {
+    landTransfer: {
       name: String,
       url: String,
     },
-  ],
+    utmSketch: {
+      name: String,
+      url: String,
+    },
+    idProof: {
+      name: String,
+      url: String,
+    },
+  },
 
   createdAt: { type: Date, default: Date.now },
 });
