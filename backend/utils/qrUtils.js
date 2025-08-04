d2fed7464cd537aa80e2dadacf98c818992b971a
almunna <124546006+almunna@users.noreply.github.com>
2025-08-04 14:36:30 +0600

    Update qrUtils.js

--- backend/utils/qrUtils.js
+++ backend/utils/qrUtils.js
@@ -4,6 +4,6 @@ import QRCode from "qrcode";
  * Generates QR image buffer from submission ID
  */
 export const getQRBuffer = async (submission_id) => {
-  const qrURL = `https://sketchplan.onrender.com/verify/${submission_id}`;
+  const qrURL = `https://sketchplan-1.onrender.com/verify/${submission_id}`;
   return await QRCode.toBuffer(qrURL, { type: "png", width: 256 });
 };
